7c70fdcecc03eafdcca4301b223a2d4cc4e08fd6
wurdz <67736923+wurdz@users.noreply.github.com>
2021-03-12 15:01:24 +0800

    fix GA tracking code

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,7 +38,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: process.env.GOOGLE_ANALYTICS_ID,
+        trackingId: "UA-191893117-1",
       },
     },
     `gatsby-plugin-sitemap`,
