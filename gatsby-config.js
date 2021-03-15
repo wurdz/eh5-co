22272353999c659a62d0868a0889dd5fd476b23d
Kee Wee <kee@wurds.org>
2021-03-15 17:52:09 +0800

    updated hero and site title

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
 
 module.exports = {
   siteMetadata: {
-    siteTitleAlt: `Minimal Blog - Gatsby Theme`,
+    siteTitleAlt: `eh5 - Cloud Ruler | Conversion copywriter`,
   },
   plugins: [
     {
